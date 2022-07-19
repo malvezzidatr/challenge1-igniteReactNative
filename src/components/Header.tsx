94491989195cd4df749ce8f907ec441c5aacac4d
caio vinicius malvezzi <caiomalvezzi101@gmail.com>
2022-07-19 10:51:26 -0300

    Resolvendo primeiro desafio da rocketSeat

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,21 +3,22 @@ import { View, Text, StyleSheet, Image } from 'react-native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 import logoImg from '../assets/images/logo/logo.png';
+import { ItemWrapper } from './ItemWrapper';
 
 interface HeaderProps {
   tasksCounter: number;
 }
 
 export function Header({ tasksCounter }: HeaderProps) {
+  const tasksCounterText = tasksCounter === 1 ? 'tarefa' : 'tarefas';
   // const tasksCounterText = TODO render 'tarefa' if tasksCounter equals 1, otherwise render 'tarefas'
   
   return (
     <View style={styles.container}>
       <Image source={logoImg} />
-      
       <View style={styles.tasks}>
         <Text style={styles.tasksCounter}>Você tem </Text>
-        {/* <Text style={styles.tasksCounterBold}>{tasksCounter} {tasksCounterText}</Text> */}
+        <Text style={styles.tasksCounterBold}>{tasksCounter} {tasksCounterText}</Text>
       </View>
     </View>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,27 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+    setTasks(oldTasks => [...oldTasks, data]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    setTasks(oldState => oldState.filter(task => {
+      if (task.id === id) {
+        task.done = !task.done;
+      }
+      return task;
+    }))
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldState => oldState.filter(task => {
+      return task.id !== id
+    }))
   }
 
   return (
